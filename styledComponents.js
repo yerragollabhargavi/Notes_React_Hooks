55309744f59a07f2afc18e798b18edc7f8e2d85b
YERRAGOLLA BHARGAVI <yeragollabhargavi@gmail.com>
2025-07-23 20:26:38 +0530

    Add files via upload

---
+++ styledComponents.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ListContainer = styled.li`
+  display: flex;
+  flex-direction: column;
+  font-family: Roboto;
+  padding: 10px;
+  margin: 6px;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 10px;
+  border-color: #d8d8d8;
+`
+
+export const Heading = styled.h1`
+  font-family: Roboto;
+  color: #1e293b;
+  
+`
+
+export const Paragraph = styled.p`
+  font-family: Roboto;
+  color: #cbd5b1;
+`
